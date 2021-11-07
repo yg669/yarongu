2c9228053119acb6041f04cc8ed3b40c64aa4f6d
Yaron <ygudes@gmail.com>
2021-11-07 18:23:10 -0500

    Wrote a socket

--- pythonTools/hello.py
+++ pythonTools/hello.py
@@ -2,5 +2,22 @@
 
 import socket
 
-print("Hello World");
+print("Hello Networ Scanenr");
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "65.61.137.117"
+port = 80
+
+def portscanner(port):
+	if sock.connect_ex((host, port)):
+		print "Port %d is closed" % (port)
+	else:
+		print "Port %d is open" % (port)
+
+portscanner(port)
+
+
+
 
