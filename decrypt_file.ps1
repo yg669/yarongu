a436024f4cfa711299de215ff13b07915e328f72
yg669 <60365438+yg669@users.noreply.github.com>
2023-11-03 15:34:00 -0400

    Add files via upload

---
+++ decrypt_file.ps1
@@ -0,0 +1,128 @@
+﻿Function Unprotect-File
+{
+[CmdletBinding(DefaultParameterSetName='SecureString')]
+[OutputType([System.IO.FileInfo[]])]
+Param(
+    [Parameter(Mandatory=$true, Position=1, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [Alias('PSPath','LiteralPath')]
+    [string[]]$FileName,
+    [Parameter(Mandatory=$false, Position=2, ValueFromPipelineByPropertyName=$true)]
+    [ValidateSet('AES','DES','RC2','Rijndael','TripleDES')]
+    [String]$Algorithm = 'AES',
+    [Parameter(Mandatory=$true, Position=3, ValueFromPipelineByPropertyName=$true, ParameterSetName='SecureString')]
+    [System.Security.SecureString]$Key,
+    [Parameter(Mandatory=$true, Position=3, ParameterSetName='PlainText')]
+    [String]$KeyAsPlainText,
+    [Parameter(Mandatory=$false, Position=4, ValueFromPipelineByPropertyName=$true)]
+    [System.Security.Cryptography.CipherMode]$CipherMode = 'CBC',
+    [Parameter(Mandatory=$false, Position=5, ValueFromPipelineByPropertyName=$true)]
+    [System.Security.Cryptography.PaddingMode]$PaddingMode = 'PKCS7',
+    [Parameter(Mandatory=$false, Position=6)]
+    [String]$Suffix, #Assigning default value in code due to it not processing ".$Algorithm" properly when Algorithm is ValueFromPipelineByPropertyName
+    [Parameter()]
+    [Switch]$RemoveSource
+)
+    Process
+    {
+        #Configure cryptography
+        try
+        {
+            if($PSCmdlet.ParameterSetName -eq 'PlainText')
+            {
+                $Key = $KeyAsPlainText | ConvertTo-SecureString -AsPlainText -Force
+            }
+
+            #Decrypt cryptography Key from SecureString
+            $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Key)
+            $EncryptionKey = [System.Convert]::FromBase64String([System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR))
+
+            $Crypto = [System.Security.Cryptography.SymmetricAlgorithm]::Create($Algorithm)
+            $Crypto.Mode = $CipherMode
+            $Crypto.Padding = $PaddingMode
+            $Crypto.KeySize = $EncryptionKey.Length*8
+            $Crypto.Key = $EncryptionKey
+        }
+        Catch
+        {
+            Write-Error $_ -ErrorAction Stop
+        }
+
+        if(-not $PSBoundParameters.ContainsKey('Suffix'))
+        {
+            $Suffix = ".$Algorithm"
+        }
+
+        #Used to store successfully decrypted file names.
+        $Files = Get-Item -LiteralPath $FileName
+
+        ForEach($File in $Files)
+        {
+            #Verify file ends with supplied suffix
+            If(-not $File.Name.EndsWith($Suffix))
+            {
+                Write-Error "$($File.FullName) does not have an extension of '$Suffix'."
+                Continue
+            }
+            
+            $DestinationFile = $File.FullName -replace "$Suffix"
+
+            Try
+            {
+                $FileStreamReader = New-Object System.IO.FileStream($File.FullName, [System.IO.FileMode]::Open)
+                $FileStreamWriter = New-Object System.IO.FileStream($DestinationFile, [System.IO.FileMode]::Create)
+
+                #Get IV from file
+                [Byte[]]$LenIV = New-Object Byte[] 4
+                $FileStreamReader.Seek(0, [System.IO.SeekOrigin]::Begin) | Out-Null
+                $FileStreamReader.Read($LenIV,  0, 3) | Out-Null
+                [Int]$LIV = [System.BitConverter]::ToInt32($LenIV,  0)
+                [Byte[]]$IV = New-Object Byte[] $LIV
+                $FileStreamReader.Seek(4, [System.IO.SeekOrigin]::Begin) | Out-Null
+                $FileStreamReader.Read($IV, 0, $LIV) | Out-Null
+                $Crypto.IV = $IV
+
+                #Peform Decryption
+                $Transform = $Crypto.CreateDecryptor()
+                $CryptoStream = New-Object System.Security.Cryptography.CryptoStream($FileStreamWriter, $Transform, [System.Security.Cryptography.CryptoStreamMode]::Write)
+                $FileStreamReader.CopyTo($CryptoStream)
+
+                #Close open files
+                $CryptoStream.FlushFinalBlock()
+                $CryptoStream.Close()
+                $FileStreamReader.Close()
+                $FileStreamWriter.Close()
+
+                #Delete encrypted file
+                if($RemoveSource){Remove-Item $File.FullName}
+
+                #Output decrypted file
+                Get-Item $DestinationFile | Add-Member –MemberType NoteProperty –Name SourceFile –Value $File.FullName -PassThru 
+                #Get-ChildItem -Path C:\PowerShellTestZone -Filter *.txt | Rename-Item -NewName { $_.Name -replace '.txt','.decrypted.txt' }
+
+            }
+            Catch
+            {
+                Write-Error $_
+                If($FileStreamWriter)
+                {
+                    #Remove failed file
+                    $FileStreamWriter.Close()
+                    Remove-Item -LiteralPath $DestinationFile -Force
+                }
+                Continue
+            }
+            Finally
+            {
+                if($CryptoStream){$CryptoStream.Close()}
+                if($FileStreamReader){$FileStreamReader.Close()}
+                if($FileStreamWriter){$FileStreamWriter.Close()}
+            }
+        }
+    }
+}
+
+#Get key from file
+$key = Get-Content -Path C:\Users\HP_HOME_PC\Source\Repos\yarongu\PS1\key.txt
+
+#Decrypt the file 
+Unprotect-File '.\files.zip.AES' -Algorithm AES -KeyAsPlainText $key #-RemoveSource
