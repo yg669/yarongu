ada1f528c0efdcd2ff03e7421af6af64c72d76b7
Yaron G <ygudes@gmail.com>
2024-06-11 13:46:53 -0400

    adding .gitignore file

--- pythonTools/hello.py
+++ pythonTools/hello.py
@@ -1,22 +1,16 @@
-#!/usr/bin/python 
 
-import socket
-
-print("Hello Networ Scanenr");
+import os
+from openai import OpenAI
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "65.61.137.117"
-port = 80
 
-def portscanner(port):
-	if sock.connect_ex((host, port)):
-		print "Port %d is closed" % (port)
-	else:
-		print "Port %d is open" % (port)
+print(openai.api_key)
 
-portscanner(port)
+
+
+
+
 
 
 
